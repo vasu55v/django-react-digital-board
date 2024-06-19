01e2f8f2a99e2146f27960963d634dad2e2d438c
vasu55v <vasuvirani55@gmail.com>
2024-06-19 15:18:34 +0530

    second commit

--- frontend/src/components/Board.js
+++ frontend/src/components/Board.js
@@ -1,15 +1,28 @@
 import React from 'react'
+import '../styles/board.css'
+import { useRef,useEffect } from 'react'
 
 const Board = () => {
+
+   const canvasRef=useRef(null);
+   const colorsRef=useRef(null);
+
+   useEffect(()=>{
+     const canvas = canvasRef.current;
+     
+   },[])
+
   return (
     <div>
-        <div className='colors'>
-             <div className='color black'></div>
-             <div className='color red'></div>
-             <div className='color green'></div>
-             <div className='color blue'></div>
-             <div className='color yellow'></div>
-        </div>
+        <canvas ref={canvasRef} className='white_board'></canvas>
+
+            <div ref={colorsRef} className='colors'>
+                <div className='color black'></div>
+                <div className='color red'></div>
+                <div className='color green'></div>
+                <div className='color blue'></div>
+                <div className='color yellow'></div>
+            </div>
     </div>
   )
 }
